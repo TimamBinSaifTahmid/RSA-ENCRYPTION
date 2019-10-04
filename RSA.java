05bfeccd78922c57bcae9862f4fcc1c492d1b7b7
TimamBinSaifTahmid <55949364+TimamBinSaifTahmid@users.noreply.github.com>
2019-10-04 19:05:02 +0600

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -71,3 +71,21 @@ public class RSA {
  }
 
 }
+
+public class RsaMain {
+     public static void main(String[] args) {
+         int a,b;
+         String ch;
+        
+        Scanner scan=new Scanner(System.in);
+        Scanner scan1=new Scanner(System.in);
+        a=scan.nextInt();
+        b=scan.nextInt();
+        ch=scan1.nextLine();
+        RSA rsa=new RSA(a, b, ch);
+        rsa.generate();
+        rsa.encripte();
+    
+    }
+    
+}
