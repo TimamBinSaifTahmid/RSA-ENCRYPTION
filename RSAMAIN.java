ac7f9fbc41b70c2e61d2bd52e1e66397427f2555
TimamBinSaifTahmid <55949364+TimamBinSaifTahmid@users.noreply.github.com>
2019-10-15 19:16:42 +0600

    Update RSAMAIN.java

--- RSAMAIN.java
+++ RSAMAIN.java
@@ -6,17 +6,20 @@ import java.util.Scanner;
 public class RsaMain {
      public static void main(String[] args) {
          int a,b;
-         String ch;
+         String ch,ch1;
         
         Scanner scan=new Scanner(System.in);
         Scanner scan1=new Scanner(System.in);
         a=scan.nextInt();
         b=scan.nextInt();
+         System.out.println("ENTER TEXT TO ENCRIPT:");
         ch=scan1.nextLine();
-        RSA rsa=new RSA(a, b, ch);
+        RSA rsa=new RSA(a, b);
         rsa.generate();
-        rsa.encripte();
-    
+        rsa.encripte(ch);
+         System.out.println("ENTER TEXT TO DECRIPT:");
+        ch1=scan1.nextLine();
+        rsa.decripte(ch1);
     }
     
 }
